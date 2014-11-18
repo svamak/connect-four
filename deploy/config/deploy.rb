ca76ad7fb7c14a107e57031f3ac5e33589d81ecb
Mantas Urnieža <mantas.urnieza@nfq.lt>
2014-11-18 11:39:17 +0200

    download composer

--- deploy/config/deploy.rb
+++ deploy/config/deploy.rb
@@ -2,7 +2,7 @@ namespace :deploy do
   desc 'Deploy to AWS server'
   after :updated, :build do
       on roles(:aws) do
-        execute "cd #{release_path} && composer install --no-dev -q"
+        execute "cd #{release_path} && php composer.phar install --no-dev -q"
         count = fetch(:keep_releases, 5).to_i
         execute "ls -1dt #{releases_path}/* | tail -n +#{count + 1} | xargs sudo rm -rf"
       end
