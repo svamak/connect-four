9ee8f647ef77a13fe7aeb95ea1302c16c9c38f54
Mantas Urnieža <mantas.urnieza@nfq.lt>
2014-11-18 11:16:59 +0200

    role is required

--- deploy/config/deploy.rb
+++ deploy/config/deploy.rb
@@ -1,8 +1,10 @@
 namespace :deploy do
   desc 'Deploy to AWS server'
   after :updated, :build do
-    run "cd #{release_path} && composer install --no-dev -q"
-    count = fetch(:keep_releases, 5).to_i
-    run :ls, "-1dt #{releases_path}/* | tail -n +#{count + 1} | xargs sudo rm -rf"
+      on roles(:aws) do
+        run "cd #{release_path} && composer install --no-dev -q"
+        count = fetch(:keep_releases, 5).to_i
+        run :ls, "-1dt #{releases_path}/* | tail -n +#{count + 1} | xargs sudo rm -rf"
+      end
   end
 end
