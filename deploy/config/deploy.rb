220faea7d50ed30235e4d041e9adaa533b8e6f08
Mantas Urnieža <mantas.urnieza@nfq.lt>
2014-11-18 10:30:52 +0200

    get rid of strange syntax

--- deploy/config/deploy.rb
+++ deploy/config/deploy.rb
@@ -1,8 +1,8 @@
 namespace :deploy do
   desc 'Deploy to AWS server'
   after :updated, :build do
-    execute "cd #{release_path} && composer install --no-dev -q"
+    run "cd #{release_path} && composer install --no-dev -q"
     count = fetch(:keep_releases, 5).to_i
-    execute :ls, "-1dt #{releases_path}/* | tail -n +#{count + 1} | xargs sudo rm -rf"
+    run :ls, "-1dt #{releases_path}/* | tail -n +#{count + 1} | xargs sudo rm -rf"
   end
 end
