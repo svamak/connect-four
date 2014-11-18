bfa9ff8f91b2eb2b29f34f3daa3d2470ba260e1e
Mantas Urnieža <mantas.urnieza@nfq.lt>
2014-11-18 11:33:01 +0200

    execute instead of run

--- deploy/config/deploy.rb
+++ deploy/config/deploy.rb
@@ -2,9 +2,9 @@ namespace :deploy do
   desc 'Deploy to AWS server'
   after :updated, :build do
       on roles(:aws) do
-        run "cd #{release_path} && composer install --no-dev -q"
+        execute "cd #{release_path} && composer install --no-dev -q"
         count = fetch(:keep_releases, 5).to_i
-        run "ls -1dt #{releases_path}/* | tail -n +#{count + 1} | xargs sudo rm -rf"
+        execute "ls -1dt #{releases_path}/* | tail -n +#{count + 1} | xargs sudo rm -rf"
       end
   end
 end
