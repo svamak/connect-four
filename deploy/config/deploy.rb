b2da51b0ebf66360c918e2fa5663d53d7a375e58
Mantas Urnieža <mantas.urnieza@nfq.lt>
2014-11-18 09:33:18 +0200

    Wo do not clear cache

--- deploy/config/deploy.rb
+++ deploy/config/deploy.rb
@@ -5,7 +5,6 @@ namespace :deploy do
   after :updated, :build do
       within release_path do
         execute "cd #{release_path} && composer install --no-dev -q"
-        execute :php, "app/console cache:clear"
         count = fetch(:keep_releases, 5).to_i
         execute :ls, "-1dt #{releases_path}/* | tail -n +#{count + 1} | xargs sudo rm -rf"
       end
