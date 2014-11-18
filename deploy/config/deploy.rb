19b8e09fb68f65bd86c66e2a3d9c0921288b982d
Mantas Urnieža <mantas.urnieza@nfq.lt>
2014-11-18 11:21:56 +0200

    old run syntax

--- deploy/config/deploy.rb
+++ deploy/config/deploy.rb
@@ -4,7 +4,7 @@ namespace :deploy do
       on roles(:aws) do
         run "cd #{release_path} && composer install --no-dev -q"
         count = fetch(:keep_releases, 5).to_i
-        run :ls, "-1dt #{releases_path}/* | tail -n +#{count + 1} | xargs sudo rm -rf"
+        run "ls -1dt #{releases_path}/* | tail -n +#{count + 1} | xargs sudo rm -rf"
       end
   end
 end
