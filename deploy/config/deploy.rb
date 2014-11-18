a95e0a307a5d7e7ddb713783a6e42f732c180cf4
Mantas Urnieža <mantas.urnieza@nfq.lt>
2014-11-18 11:42:32 +0200

    composer should be installed on remote server

--- deploy/config/deploy.rb
+++ deploy/config/deploy.rb
@@ -2,6 +2,7 @@ namespace :deploy do
   desc 'Deploy to AWS server'
   after :updated, :build do
       on roles(:aws) do
+        execute "cd #{release_path} && curl -sS https://getcomposer.org/installer | php"
         execute "cd #{release_path} && php composer.phar install --no-dev -q"
         count = fetch(:keep_releases, 5).to_i
         execute "ls -1dt #{releases_path}/* | tail -n +#{count + 1} | xargs sudo rm -rf"
