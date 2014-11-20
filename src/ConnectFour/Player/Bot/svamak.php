80cc34cffd3f0db76dbc920da0e4b3e14b7d5afb
Svajūnas Maksvytis <svajunas.maksvytis@gmail.com>
2014-11-20 15:42:50 +0200

    Create svamak.php

---
+++ src/ConnectFour/Player/Bot/svamak.php
@@ -0,0 +1,79 @@
+<?php
+
+
+namespace ConnectFour\Player\Bot;
+
+
+use ConnectFour\Game\Grid\Helper;
+use ConnectFour\Player\PlayerInterface;
+
+class svamak implements PlayerInterface
+{
+    /**
+     * Returns next move of player by given grid
+     *
+     * <code>
+     * $grid = [
+     *    [0,0,0,1,2,2,0],
+     *    [0,0,0,1,2,0,0],
+     *    [0,0,0,0,1,0,0],
+     *    [0,0,0,0,0,0,0],
+     *    [0,0,0,0,0,0,0],
+     *    [0,0,0,0,0,0,0],
+     * ]
+     * </code>
+     *
+     * @param array $grid Grid representation in 7 columns, 6 rows array
+     *
+     * @return int 0 to 6 index of column to drop disk
+     */
+    public function move($grid)
+    {
+        $columns = Helper::getAvailableColumns($grid);
+        $bestColumn = $this->bestInColumns($columns);
+		return $bestColumn;
+    }
+	
+	public function goodInSingleColumn($column){
+		//idea: count 1's in a single occurence
+		//1 1 2 = 2
+		//2 1 2 1 1 = 0
+		$goodOne = 0;
+		foreach($column as $index){
+			if($index == 1){
+				$goodOne++;
+			}else{
+				$goodOne = 0;
+			}
+		}
+		return $goodOne;
+	}
+	
+	public function bestInColumns($columns){
+	//return column index with biggest success
+		$results = [];
+		$i = 0;
+		foreach($columns as $column){
+			$results [] = ["amount" => $this->goodInSingleColumn($column), "columnIndex" => $i];
+			$i++;
+		}
+		
+		$sorted = $this->sortByKeyValue($results, "amount");
+		
+		return $results[0]["columnIndex"];
+	}
+	
+	public function sortByKeyValue($data, $sortKey, $sort_flags = SORT_DESC){
+		if (empty($data) or empty($sortKey)) return $data;
+
+		$ordered = array();
+		foreach ($data as $key => $value)
+			$ordered[$value[$sortKey]] = $value;
+
+		ksort($ordered, $sort_flags);
+
+		return array_values($ordered); 
+	}
+	
+	
+}
